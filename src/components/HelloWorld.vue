88fd2355865271305f796195364d03ac7c84a798
MFukazawa <micahfukazawa@gmail.com>
2020-12-15 14:40:09 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="increase">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <attrs id="attrs-id" class="class-test">Attrs component</attrs>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import attrs from "./attrs.vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const count = ref(0);
+
+    const increase = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      increase,
+    };
+  },
+  components: {
+    attrs,
+  },
+});
+</script>
+
+<style scoped>
+.class-test {
+  /* border: 1px solid red; */
+  background-color: red;
+  padding: 3rem;
+  display: block;
+}
+</style>
---
+++ src/components/attrs.vue
@@ -0,0 +1,23 @@
+<template>
+  <label>
+    <h1>{{ title }}</h1>
+    <input type="text" v-bind="$attrs" v-model="title"/>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const title = ref("Input Test")
+    return {
+      title
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
